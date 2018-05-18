1561edda6f17debcb1b84d0cc4711988b30aa45d
github username <github email address>
2018-05-18 16:15:00 -0500

    setting up home page stuff.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
 
   def index
+    #may not work because home page is not in a folder
+    render 'home'
   end
 end
--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -6,11 +6,14 @@ class CreditCard < ApplicationRecord
 
   def calculate_current_balance
     #calculates current_balance by using payments, charges, date, apr
-
+    #should be triggered every time:
+      #--a new Charge is created
+      #--a new Payment is created (payments are always done against principal for first 30 days, then against current_balance)
+      #--30 days have passed
   end
 
   def calculate_daily_interest
-    
+    #calculates but does not apply daily interest
   end
 
 end
