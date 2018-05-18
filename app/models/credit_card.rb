1305b017c2e3bd457020844058bcb3abea83b053
github username <github email address>
2018-05-18 16:17:14 -0500

    controllers up

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -5,6 +5,7 @@ class CreditCard < ApplicationRecord
   #apr, current_balance, credit_limit, opening_date
 
   def calculate_current_balance
+    
     #calculates current_balance by using payments, charges, date, apr
     #should be triggered every time:
       #--a new Charge is created
