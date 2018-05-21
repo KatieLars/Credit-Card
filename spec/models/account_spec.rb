7efa1f2227f82279f606951cee31d049905f2877
github username <github email address>
2018-05-21 14:57:44 -0500

    remove pry

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 RSpec.describe Account, type: :model do
   before(:each) do
-  @account = Account.create(apr: 35, credit_limit: 1000, opening_date: Date.today, current_balance: 500) #date is 30 days from today
+  @account = Account.create(apr: 35, credit_limit: 1000, opening_date: Date.today.days_ago(30), current_balance: 500) #date is 30 days from today
   @first_transaction = @account.transactions.build(amount: -200)
   @first_transaction.date = @account.opening_date.days_since(15)
   @first_transaction.balance
@@ -23,13 +23,12 @@ end
 
 describe ":interest_from_last_transaction" do
   it "calculates the total interest from today to the date of the last transaction" do
-    binding.pry
     last_trans_interest = @account.interest_from_last_transaction.round(2)
     expect(last_trans_interest).to eq(1.92)
   end
 end
 
-describe ":total_interest"
+describe ":total_interest" do
   it "calculates the total interest of all transactions" do
     all_interest = @account.total_interest.round(2)
     expect(all_interest).to eq(11.99)
